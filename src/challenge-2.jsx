d017daf7cca68c37e6a3b3baf8017cc56f552802
wellpence5 <alvinmuthomi22@gmai.com>
2025-07-21 21:50:46 +0300

    Fixed json

--- src/challenge-2.jsx
+++ src/challenge-2.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
-const API_URL = 'http://localhost:3000/goals';
+const API_URL = '/goals';
+
 
 function SmartGoalPlanner() {
   const [goals, setGoals] = useState([]);
